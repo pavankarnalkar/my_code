c60c3a1726290d2426cf5c33b2cd49115f52d1d4
pavankarnalkar <49977380+pavankarnalkar@users.noreply.github.com>
2021-04-25 20:11:55 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
+import dotenv from "dotenv";
+dotenv.config();
 // function index() {
 //   return <div></div>;
 // }
